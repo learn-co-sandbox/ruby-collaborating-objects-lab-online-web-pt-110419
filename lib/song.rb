cfac9438d104f8582cbccf22a78a7d52a3cd08f3
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-12 18:47:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def self.new_by_filename(filename)
     artist_name, song_name, extra = filename.split(" - ")
     song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
+    artist = Artist.find_or_create_by_name(name)
     artist.add_song(song)
   end
 end

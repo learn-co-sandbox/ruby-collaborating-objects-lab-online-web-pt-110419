f4781cc52f4b7aa819b961a8eb4ea18002a4c1ec
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-12 19:15:22 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,24 +1,44 @@
 class MP3Importer
-  attr_accessor :path
 
-  def initialize(path)
-    @path = path
-  end
+    attr_accessor :path
 
-  def files
-    files = []
-    Dir.new(self.path).each do |file|
-      files << file if file.length > 4
+    def initialize(path)
+        @path = path
     end
-    files
-  end
 
-  def import
-    self.files.each do |filename|
-      Song.new_by_filename(filename)
+    def files
+        files ||= Dir["#{@path}/*"].collect {|song| song.gsub("#{@path}/", "")}
     end
-  end
+
+    def import
+       files.each {|song| Song.new_by_filename(song)}
+    end
+
 end
+
+
+
+# class MP3Importer
+#   attr_accessor :path
+
+#   def initialize(path)
+#     @path = path
+#   end
+
+#   def files
+#     files = []
+#     Dir.new(self.path).each do |file|
+#       files << file if file.length > 4
+#     end
+#     files
+#   end
+
+#   def import
+#     self.files.each do |filename|
+#       Song.new_by_filename(filename)
+#     end
+#   end
+# end
 
 
 # class MP3Importer

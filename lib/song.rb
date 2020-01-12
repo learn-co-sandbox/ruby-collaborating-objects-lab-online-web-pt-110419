f3fe265f393d3b9de093074bcc6c48211ee8094d
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-12 19:03:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,6 @@ class Song
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-     @artist = song_artist
-      #@artist.add_song(self)
     artist.add_song(self)
   end
 
@@ -19,6 +17,29 @@ class Song
     song
   end
 end
+
+
+# class Song
+#   attr_accessor :artist, :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def artist_name=(name)
+#     self.artist = Artist.find_or_create_by_name(name)
+#     @artist = song_artist
+#       #@artist.add_song(self)
+#     artist.add_song(self)
+#   end
+
+#   def self.new_by_filename(file)
+#     song_info = file.chomp(".mp3").split(" - ")
+#     song = Song.new(song_info[1])
+#     song.artist_name = song_info[0]
+#     song
+#   end
+# end
 
 # class Song
 #   attr_accessor :name, :artist
